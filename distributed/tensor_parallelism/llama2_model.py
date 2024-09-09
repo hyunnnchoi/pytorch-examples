ed7b89fabe665ab47c0fc56613d3d640d92e86df
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2024-09-09 21:18:18 +0900

    Update llama2_model.py

--- distributed/tensor_parallelism/llama2_model.py
+++ distributed/tensor_parallelism/llama2_model.py
@@ -34,17 +34,17 @@ from torch.distributed.fsdp.wrap import (
 
 @dataclass
 class ModelArgs:
-    dim: int = 4096
-    n_layers: int = 32
-    n_heads: int = 32
+    dim: int = 512
+    n_layers: int = 6
+    n_heads: int = 8
     n_kv_heads: Optional[int] = None
     vocab_size: int = -1  # defined later by tokenizer
     multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
     ffn_dim_multiplier: Optional[float] = None
     norm_eps: float = 1e-5
 
-    max_batch_size: int = 32
-    max_seq_len: int = 32768
+    max_batch_size: int = 1
+    max_seq_len: int = 1024
     # If `True`, then each transformer block init uses its layer ID, and if
     # `False`, each uses the total number of transformer blocks
     depth_init: bool = True
